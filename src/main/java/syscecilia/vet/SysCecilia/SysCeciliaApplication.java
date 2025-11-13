f891aeb7c396af2eaf351b932f6b358313c72b76
Jessica Machado <machadojessica1997@gmail.com>
2025-11-13 19:00:57 -0300

    feat: Importação inicial do projeto

---
+++ src/main/java/syscecilia/vet/SysCecilia/SysCeciliaApplication.java
@@ -0,0 +1,13 @@
+package syscecilia.vet.SysCecilia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysCeciliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysCeciliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/syscecilia/vet/SysCecilia/SysCeciliaApplicationTests.java
@@ -0,0 +1,13 @@
+package syscecilia.vet.SysCecilia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysCeciliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
